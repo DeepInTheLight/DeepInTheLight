93072329f4322e6b5ef8b380a6e9d9dc4052bbff
Raphael Catolino <raphael.catolino@gmail.com>
2013-02-16 12:49:06 +0100

    Add World and Element classes
    Signed-off-by: Raphael Catolino <raphael.catolino@gmail.com>

---
+++ src/deepinthelight/World.java
@@ -0,0 +1,19 @@
+package deepinthelight;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import java.util.ArrayList;
+
+public class World {
+
+  private ArrayList<Element> Elements;
+
+  public World() {
+    Elements = new ArrayList();
+  }
+
+}
