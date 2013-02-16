c78c051f06925400598a6a57339081467bb6ec69
Raphael Catolino <raphael.catolino@gmail.com>
2013-02-16 17:35:36 +0100

    Screen add populate and getNextScreen
    Signed-off-by: Raphael Catolino <raphael.catolino@gmail.com>

--- src/deepinthelight/Screen.java
+++ src/deepinthelight/Screen.java
@@ -2,11 +2,14 @@ package deepinthelight;
 
 import java.util.HashMap;
 
+import org.newdawn.slick.geom.Shape;
+
 public class Screen {
 
     public static HashMap<String, Screen> ScreenMap = null;
     private final int maxObstacleSize = 20;
     private int obstacleSize = 0;
+    private boolean populated;
 
     public enum Zone {
         UP, DOWN, LEFT, RIGHT
@@ -22,7 +25,7 @@ public class Screen {
         this.bottom = bottom;
         this.left = left;
         this.right = right;
-
+        populated = false;
         ScreenMap.put(this.serialize(), this);
     }
 
@@ -34,6 +37,26 @@ public class Screen {
         ScreenMap = new HashMap<String, Screen>(100);
         return new Screen(screenY, screenY + Main.height,
                           screenX, screenX + Main.height);
+    }
+
+    public Screen getNextScreen(Gunther gunther) {
+        Shape box = gunther.getBox();
+        Screen nextScreen = null;
+        if (box.getCenterY() > top) {
+            nextScreen = getNextScreen(Zone.UP);
+        } else if (box.getCenterY() < bottom) {
+            nextScreen = getNextScreen(Zone.DOWN);
+        } else if (box.getCenterX() < left) {
+            nextScreen = getNextScreen(Zone.LEFT);
+        } else if (box.getCenterX() > right) {
+            nextScreen = getNextScreen(Zone.RIGHT);
+        }
+
+        if (!nextScreen.isInScreen(gunther)) {
+            return nextScreen.getNextScreen(gunther);
+        }
+
+        return nextScreen;
     }
 
     public Screen getNextScreen(Zone where) {
@@ -57,6 +80,31 @@ public class Screen {
         }
 
         return getScreen(nexttop, nextbottom, nextleft, nextright);
+    }
+
+    public boolean isInScreen(Gunther gunther) {
+        Shape box = gunther.getBox();
+        if (box.getCenterY() > top || box.getCenterY() < bottom ||
+            box.getCenterX() < left || box.getCenterX() > right) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void populateNeighbors() {
+        this.getNextScreen(Zone.UP).populate();
+        this.getNextScreen(Zone.DOWN).populate();
+        this.getNextScreen(Zone.LEFT).populate();
+        this.getNextScreen(Zone.RIGHT).populate();
+    }
+
+    public void populate() {
+        if (populated) {
+            return;
+        }
+
+        populated = true;
     }
 
     private Screen getScreen(float top, float bottom, float left, float right) {
