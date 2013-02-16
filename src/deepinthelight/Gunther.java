a41052fa1ae8c774c8279693b97a5004bd385e5a
peniblec <kevin.lg@free.fr>
2013-02-16 13:59:49 +0100

    gunther should now be moving

--- src/deepinthelight/Gunther.java
+++ src/deepinthelight/Gunther.java
@@ -10,18 +10,20 @@ package deepinthelight;
  */
 public class Gunther extends Element {
 
-    public enum Direction{
-        NONE, UP, DOWN, LEFT, RIGHT
+    public enum Direction {
+        NONE, LEFT, RIGHT, UP, DOWN,
+            LEFTUP, LEFTDOWN, RIGHTUP, RIGHTDOWN
     }
-    
+
 
 
     private int energyLeft;
     private final int BASE_ENERGY = 42;
     private final int ENERGY_DEC = 5;
 
-    private Direction currentDirection = Direction.NONE;
-    private final int SPEED = 5;
+    private Direction currentDir = Direction.NONE;
+    private final float SPEED = 5;
+    private final float DIAG_SPEED = SPEED/(float)java.lang.Math.sqrt(2);
 
     @Override
     public void update() {
@@ -38,8 +40,43 @@ public class Gunther extends Element {
 
     }
 
-    public void move(Direction dir) {
-        currentDirection = dir;
+    public void move(Direction newDir) {
+        currentDir = newDir;
+        
         //TODO: actually move
+        float curX = box.getCenterX();
+        float curY = box.getCenterY();
+        
+        switch(currentDir) {
+        case LEFT :
+            box.setCenterX( curX - SPEED );
+            break;
+        case RIGHT :
+            box.setCenterX( curX + SPEED );
+            break;
+        case DOWN :
+            box.setCenterY( curY - SPEED );
+            break;
+        case UP :
+            box.setCenterY( curY + SPEED );
+            break;
+        case LEFTUP :
+            box.setCenterX( curX - DIAG_SPEED );
+            box.setCenterY( curY + DIAG_SPEED );
+            break;
+        case LEFTDOWN :
+            box.setCenterX( curX - DIAG_SPEED );
+            box.setCenterY( curY - DIAG_SPEED );
+            break;
+        case RIGHTUP :
+            box.setCenterX( curX + DIAG_SPEED );
+            box.setCenterY( curY + DIAG_SPEED );
+            break;
+        case RIGHTDOWN :
+            box.setCenterX( curX + DIAG_SPEED );
+            box.setCenterY( curY - DIAG_SPEED );
+            break;
+        }
+        
     }
 }
