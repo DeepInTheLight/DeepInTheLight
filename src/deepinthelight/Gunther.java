109df6f64ac031d209b052293d67ab65c729d1f7
peniblec <peniblec@peniblec-laptop.(none)>
2013-02-16 12:51:34 +0100

    say hello to Gunther

---
+++ src/deepinthelight/Gunther.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deepinthelight;
+
+/**
+ *
+ * @author peniblec
+ */
+public class Gunther extends Element {
+
+    int energyLeft;
+
+    public static void update() {
+
+    }
+
+    public static void render() {
+
+    }
+
+    public static void collide() {
+
+    }
+}
